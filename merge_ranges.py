b3f42fcb5e846131367882b12bbab19ba7b261e4
TrimChap <trimchap@trishtunney.com>
2020-08-19 20:29:20 -0700

    added some comments

--- merge_ranges.py
+++ merge_ranges.py
@@ -15,10 +15,13 @@ def merge_ranges(ranges):
         current = ranges[i]
         # print(current[1],' >= ',ranges[i+1][0],' ?')
         while i < end and (current[1]) >= (ranges[i+1][0]):
+            # merge these two meetings
             current = merge_these(current,ranges[i+1])
-            i +=1
+            i +=1 # move to the next and see if it overlaps with them too
+        # current = either the merged meeting or the next one that doesn't overlap
         merged_list.append(current)
         i +=1
+        # add the last meeting to the merged list if it wasn't part of the merge
         if i == end and ranges[i] != merged_list[-1]:
             merged_list.append(ranges[i])
 
@@ -32,5 +35,15 @@ print("--------")
 ranges = [(0,1),(3,5),(4,8),(10,12),(9,10),(8,13)]
 print("merged list: ", merge_ranges(ranges))
 print("--------")
+ranges = [(1, 10), (2, 6), (3, 5), (7, 9)]
+print("merged list: ", merge_ranges(ranges))
+print("--------")
 ranges = [(0,1),(3,4),(4,5),(11,12),(9,10),(8,9),(13,14)]
 print("merged list: ", merge_ranges(ranges))
+print("--------")
+ranges = [(1, 2), (2, 3)]
+print("merged list: ", merge_ranges(ranges))
+print("--------")
+ranges =  [(1, 5), (2, 3)]
+print("merged list: ", merge_ranges(ranges))
+  
