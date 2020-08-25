03f4436d93c30c8553e9f46f078ecd6281888988
TrimChap <trimchap@trishtunney.com>
2020-08-24 21:58:19 -0700

    reversed string

--- reverse_words.py
+++ reverse_words.py
@@ -1,4 +1,4 @@
-def reverse_words(message):
+def reverse_words_not_in_place(message):
     result = ''
     next_word = ''
 
@@ -12,8 +12,16 @@ def reverse_words(message):
 
     return result
 
+def reverse_string_in_place(msg):
+    for i in range(int(len(msg)//2)):
+        msg[i],msg[-1-i] = msg[-1-i],msg[i]
+    return msg
+    
+
 # Test
 message = ['c','a','k','e',' ',
         'p','o','u','n','d',' ',
         's','t','e','a','l']
-print(reverse_words(message))
+# print(reverse_words(message))
+msg = reverse_string_in_place(message)
+print(''.join(msg))
